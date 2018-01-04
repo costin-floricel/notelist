2fddc53defdc0b03bc0201a2730099d96728c9a9
Costin Floricel <costin_floricel@hotmail.com>
2018-01-04 20:50:33 +0100

    Finished the banner section, login, register and add note section

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
 
 
 gulp.task('styles', function(){
 	return gulp.src('./app/styles/styles.css')
-	.pipe(postcss([cssvars,nested, autoprefixer]))
+	.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
 	.pipe(gulp.dest('./app/styles/css/'));
 });
 
